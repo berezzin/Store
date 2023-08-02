fbb06f5e8eeef8f4efc57cd6ba7c83c6e4be2974
berezzin <biarezzin@gmail.com>
2023-08-02 21:43:51 +0200

    Added login_required injection

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -37,6 +38,7 @@ def register(request):
     return render(request, 'users/register.html', context=context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
