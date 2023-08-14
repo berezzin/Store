57256a94076bb43c0f5973bfc0a314c893e21190
berezzin <biarezzin@gmail.com>
2023-08-14 13:03:11 +0200

    Configurated django admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from products.models import Category, Product
+from products.models import Category, Product, Basket
 
-admin.site.register([Category, Product])
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'quantity')
+
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+
+    search_fields = ('name',)
+
+    ordering = ('name',)
+
+
+class BasketInline(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
+
+
+admin.site.register([Category])
